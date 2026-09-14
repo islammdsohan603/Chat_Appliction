d247d44910a384bfbaef43a25fea2b372f387a59
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-14 22:11:17 +0600

    server started

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Chat Application Backend is Running");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
